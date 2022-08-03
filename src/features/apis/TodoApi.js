663b3f9d6cbfab69a521381be69bd3face09f1ad
Laughinggg <953651397@qq.com>
2022-08-03 16:43:45 +0800

    feat: deleteTodo & doneTodo

--- src/features/apis/TodoApi.js
+++ src/features/apis/TodoApi.js
@@ -7,3 +7,11 @@ export function getTodo() {
 export function addTodoApi(newTodo) {
   return api.post("/todos", newTodo);
 }
+
+export function deleteTodoApi(todoId) {
+  return api.delete("/todos/" + todoId);
+}
+
+export function doneTodoApi(todo) {
+  return api.put("/todos/" + todo.id, { done: !todo.done });
+}
--- src/features/todoList/TodoItem.js
+++ src/features/todoList/TodoItem.js
@@ -1,19 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTodo, todoDone } from "./todoListSlice";
 import "./style.css";
+import { deleteTodoApi, doneTodoApi } from "../apis/TodoApi";
 export default function TodoItem(props) {
   const todo = useSelector((state) =>
     state.todoListSlice.todos.find((todo) => todo.id === props.id)
   );
   const dispatch = useDispatch();
   const remove = () => {
-    dispatch(deleteTodo(props.id));
+    deleteTodoApi(todo.id).then(() => {
+      dispatch(deleteTodo(todo.id));
+    });
   };
+
+  const doneTodo = () => {
+    doneTodoApi(todo).then((res) => {
+      dispatch(todoDone(res.data.id));
+    });
+  };
+
   return (
     <div>
       <button
         className={todo.done === true ? "todoItem-done" : "todoItem-notdone"}
-        onClick={() => dispatch(todoDone(props.id))}
+        onClick={doneTodo}
       >
         {todo.text}
       </button>
